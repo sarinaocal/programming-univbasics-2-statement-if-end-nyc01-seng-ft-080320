35d328e69d4587b19c414eeed8db7c94ad5d0d19
Sarina Ocal <sarinaocal@gmail.com>
2020-06-29 01:45:37 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,12 @@
 # Write your solution here
+this_year = 2020
+puts "Hey, it's not 2019!" unless this_year == 2020
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
