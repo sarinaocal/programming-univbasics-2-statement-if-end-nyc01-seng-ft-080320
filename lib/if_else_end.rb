45ac2e5008a551ae220ae24ce6b3e57a9cdbef63
Sarina Ocal <sarinaocal@gmail.com>
2020-06-29 01:40:52 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,7 +5,7 @@ puts "Hey, it's not 2019!" unless this_year == 2020
 current_time = Time.now
 current_time = current_time.to_i
 
-if current_time % 2 == 0
+if Time.now.to_i % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
